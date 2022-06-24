2d57698ee237c71416d59d853572e9da061cc70b
garzoku <garzoku@WGC10316TQCB3.zld.dc.ford.com>
2022-06-24 17:50:39 -0500

    complete counter using tdd

---
+++ src/AwesomeCounter.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react';
+
+const AwesomeCounter = ({initialValue}) => {
+    // define our state
+    const [count, setCount] = useState(initialValue ?? 0);
+    const add = () => {
+        setCount((prevCount)=>prevCount + 1);
+    };
+
+    const remove = () => {
+        setCount((prevCount)=>{
+            const result = prevCount - 1
+            if(result < 0){
+                return 0;
+            }
+            return result;
+        });
+    };
+
+  return (
+    <div>
+        <h1>Awesome Counter</h1>
+        <button onClick={remove}>Remove</button>
+        <span>{count}</span>
+        <button onClick={add}>Add</button>
+    </div>
+  )
+}
+
+export default AwesomeCounter
